483d4b9079e96d82b519f4785fb3a07ed1e16385
mariya <mariasycheva1992@gmail.com>
2023-12-23 17:48:51 +0300

    Задача 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+internal class Program
+{
+    static int[] fillArray(int countElement = 10)
+    {
+        int[] arrayFill = new int[countElement];
+        Random rand = new Random();
+        for (int i = 1; i < countElement; i++)
+            {
+                arrayFill[i] = rand.Next(0, 100);
+            }
+        return arrayFill;
+    }
+    static void printArray(int[] arr){
+        for(int i = 1; i < arr.GetLength(0); i++){   
+            Console.WriteLine(arr[i]);
+        }
+    }
+    static int GetCountElement(int[] arr)
+    {
+        int count = 0;
+        for(int i = 0; i < arr.GetLength(0); i++){
+            if(arr[i] >= 20 && arr[i] <= 90){
+                count ++;
+            }
+        }
+        return count;
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(GetCountElement(fillArray(10)));
+    }
+}
